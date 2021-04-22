3b955e91cf42a34548613e26ee22e74617ca7910
Thays <thayscasado@hotmail.com>
2021-04-22 01:35:57 -0700

    4. variaveis

---
+++ Discover/Guia_Estelares/O_guia_estelar_de_JavaScript/4_variaveis/script.js
@@ -0,0 +1,17 @@
+// variables
+  // var = global and local
+    // hoisting
+  // let = local
+  // const = local
+    // dont change
+    // can -> same name if different scope
+
+// scope
+  // global
+  // local
+
+// Naming
+  // CAN  
+    // Iniciate: $_
+  // CAN NOT
+    // Iniciate: numbers, empty space
