d763f89945a538ae3c22f734a245baf65c1df398
Thays <thayscasado@hotmail.com>
2021-04-22 01:13:14 -0700

    3 tipos de dados

---
+++ Discover/Guia_Estelares/O_guia_estelar_de_JavaScript/2_Primeiros_passos/script.js
@@ -0,0 +1,58 @@
+console.log('teste');
+
+// comentario linha
+/*
+    Comentario bloco
+
+*/
+
+// string
+// number
+    // int
+    // float
+    //  NaN
+    // Infinity
+// boolean
+    // true
+    // false
+// undefined vs null
+// object
+    // attributes
+    // methods
+    // {attribute: "value"}
+    console.log({
+        name: "Mayk",
+        idade: 36,
+        andar: function(){
+            console.log('andando')
+        }
+    })
+
+// array / vetores
+    // a list/ grouping data
+    // ["Mayk", 36]
+    console.log([
+        "Leite",
+        "Ovos",
+        2,
+        3,
+    ])
+
+// Data types
+    // primitive
+        // String
+        // Number
+        // Boolean
+        // undefined
+        // Symbol ?
+        // BigInt ?
+    // Structural
+        // Object
+            // Array
+            // Map
+            // Set
+            // Date
+            // ...
+        // Function
+    // Structural Root primitive
+        // null
